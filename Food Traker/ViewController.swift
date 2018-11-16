7aab5877da81ece092e71e2605547d35900b76be
Jafer Pompeu <jafer@MacBook-Pro-de-Jafer.local>
2018-11-16 17:55:24 -0300

    Initial Commit

---
+++ Food Traker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Food Traker
+//
+//  Created by Jafer Daltro Pompeu Jr on 16/11/18.
+//  Copyright © 2018 Jafer Daltro Pompeu Jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
